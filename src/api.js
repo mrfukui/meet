42f446ad18adf211513b5270363c4793afdb0500
mrfukui <mr.fukui@yahoo.com>
2024-01-30 18:13:17 -0800

    Edited api.js

--- src/api.js
+++ src/api.js
@@ -80,20 +80,23 @@ export const getAccessToken = async () => {};
 
 const accessToken = localStorage.getItem("access_token");
 
-const tokenCheck = accessToken && (await checkToken(accessToken));
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem("access_token");
+  const tokenCheck = accessToken && (await checkToken(accessToken));
 
-if (!accessToken || tokenCheck.error) {
-  await localStorage.removeItem("access_token");
-  const searchParams = new URLSearchParams(window.location.search);
-  const code = await searchParams.get("code");
-  if (!code) {
-    const response = await fetch(
-      "Yhttps://j7phfrbk88.execute-api.us-west-1.amazonaws.com/dev/api/get-auth-url"
-    );
-    const result = await response.json();
-    const { authUrl } = result;
-    return (window.location.href = authUrl);
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch(
+        "https://j7phfrbk88.execute-api.us-west-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
   }
-  return code && getToken(code);
-}
-return accessToken;
+  return accessToken;
+};
