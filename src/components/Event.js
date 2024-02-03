d45e29889f2a05db280c7b968039db26446a22f0
mrfukui <mr.fukui@yahoo.com>
2024-02-02 20:00:19 -0800

    added className to Event.js file

--- src/components/Event.js
+++ src/components/Event.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false);
   return (
-    <li>
+    <li className="event">
       <h2>{event.summary}</h2>
       <p>{event.created}</p>
       <p>{event.location}</p>
