dcc3624363b41d1481ade945d90176c2c30f9878
mrfukui <mr.fukui@yahoo.com>
2024-02-06 18:43:22 -0800

    added Atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('ef442b5f48e049bdbb207b6551e42b46').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
