0220ff017818c5bc167ad7aa170722eb0af74f27
mrfukui <mr.fukui@yahoo.com>
2024-02-15 16:07:59 -0800

    removed NProgress function

--- src/api.js
+++ src/api.js
@@ -62,7 +62,6 @@ export const getEvents = async () => {
 
   if (!navigator.onLine) {
     const events = localStorage.getItem("lastEvents");
-    NProgress.done();
     return events ? JSON.parse(events) : [];
   }
 
@@ -77,7 +76,6 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
-      NProgress.done();
       localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
